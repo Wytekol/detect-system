8853044f4e197f0560f485b9155556d65461ad4d
Wytekol <101928745+Wytekol@users.noreply.github.com>
2022-08-29 09:56:36 +0200

    Update os.h

--- os.h
+++ os.h
@@ -1,22 +1,37 @@
 // this is the main h file. include it, and you're ready-to-go!
 
-typedef enum System {
-    WINDOWS,
-    LINUX,
-    MACOS,
-    FREEBSD,
-    ERROR
-} System;
+#define System char*
+#define _WINDOWS "Windows"
+#define _LINUX   "Linux"
+#define _MACOS   "MacOS"
+#define _FREEBSD "FreeBSD"
+#define _ANDROID "Android"
+#define _AMIGAOS "AmigaOS"
+#define _AEGIS   "Apollo AEGIS"
+#define _BEOS    "BeOS"
+#define _BLUEGENE "Blue Gene"
+#define _ERROR NULL
+
 
 System GetOS(void) {
     #if defined(_WIN32)
-      return WINDOWS;
-    #elif defined(__linux__)
-      return LINUX;
+          return WINDOWS;
+    #elif defined(__ANDROID__)
+          return _ANDROID;
     #elif defined(__FreeBSD__)
-      return FREEBSD;
+          return _FREEBSD;
     #elif defined(__APPLE__)
-      return MACOS;
+          return _MACOS;
+    #elif defined(__linux__)
+          return _LINUX;
+    #elif defined(UTS)
+          return _AMDAHLUTS;
+    #elif defined(AMIGA)
+          return _AMIGAOS;
+    #elif defined(aegis)
+          return _AEGIS;
+    #elif defined(__bg__)
+          return _BLUEGENE;
     #endif
-    return ERROR;
+    return _ERROR;
 }
