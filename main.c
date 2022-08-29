f1603f0c096b0fff808680a79f065a777f520220
Wytekol <101928745+Wytekol@users.noreply.github.com>
2022-08-29 08:48:22 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+/* this .c file only is an example how to use this header file. Download only the os.h file. */
+#include "os.h"
+#include <stdio.h>
+
+int main(void) {
+    System os = GetOS();
+    if (os == WINDOWS) {
+        printf("You're on Windows!");
+    } else if (os == LINUX) {
+        printf("You're on Linux!");
+    } else if (os == MACOS) {
+        printf("You're on MacOS!");
+    } else if (os == FREEBSD) {
+        printf("You're on FreeBSD!");
+    } else {
+        printf("Operating system not found.");
+    }
+    return 0;
+}
